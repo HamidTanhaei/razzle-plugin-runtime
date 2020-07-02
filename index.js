b35655f9599f9999008c19587adaf803b246b435
Hamid Tanhaei <hamid_rainy@yahoo.com>
2020-07-02 16:09:42 +0430

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const Dotenv = require("dotenv");
 module.exports = function myRazzlePlugin(config, env, webpack, options) {
     const { target, dev } = env;
     
-    console.log('here the plugin', target, dev);
-    
     if (target === 'web') {
         //get env filename
         let envFilename = '.env';
